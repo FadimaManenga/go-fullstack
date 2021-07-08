4d58bedacf1b84375c273fb63d52bb3f77fa467a
git504 <git504@mailo.com>
2021-07-08 17:41:20 +0200

    first commit

--- models/Thing.js
+++ models/Thing.js
@@ -1,15 +1,14 @@
 // Importation de mongoose
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 // Creation d'un schéma Thing
 const thingSchema = mongoose.Schema({
-	title: {type: String, required: true}, 
-	description: { type: String, required: true},
-	imageUrl: {type: String, required: true},
-	userId: {type: String, required: true},
-	price: {type: Number, required: true},
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
 });
 
 // Exporter le modele
-module.exports = mongoose.model('Thing', thingSchema);
+module.exports = mongoose.model("Thing", thingSchema);
